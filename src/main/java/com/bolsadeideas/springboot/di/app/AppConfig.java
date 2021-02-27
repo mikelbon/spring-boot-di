c7e6c1951165c6c2f79f65fca018867dccb2db54
Miguel Carhuas <mikel_ct@hotmail.com>
2021-02-26 22:13:56 -0500

    domain services

--- src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -2,13 +2,22 @@ package com.bolsadeideas.springboot.di.app;
 
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+import com.bolsadeideas.springboot.di.app.models.service.MiServicioComplejo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
     @Bean("miServicioSimple")
+    @Primary
     public IServicio registrarMiServicio(){
         return new MiServicio();
+    }
+
+    @Bean("miServicioComplejo")
+
+    public IServicio registrarMiServicioComplejo(){
+        return new MiServicioComplejo();
     }
 }
