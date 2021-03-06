7078f74fcf19f2b73b5338c9cbe7fef0d5d48d5e
Miguel Carhuas <mikel_ct@hotmail.com>
2021-03-05 23:21:52 -0500

    Add differents scopes

--- src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.di.app;
 
+import com.bolsadeideas.springboot.di.app.models.domain.ItemFactura;
+import com.bolsadeideas.springboot.di.app.models.domain.Producto;
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicioComplejo;
@@ -7,17 +9,45 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class AppConfig {
     @Bean("miServicioSimple")
     @Primary
-    public IServicio registrarMiServicio(){
+    public IServicio registrarMiServicio() {
         return new MiServicio();
     }
 
     @Bean("miServicioComplejo")
 
-    public IServicio registrarMiServicioComplejo(){
+    public IServicio registrarMiServicioComplejo() {
         return new MiServicioComplejo();
+    }
+
+    @Bean("itemsFactura")
+    public List<ItemFactura> registrarItems() {
+        Producto producto1 = new Producto("Camara Sony", 100);
+        Producto producto2 = new Producto("Bicicleta Bianchi Aro 26", 200);
+
+        ItemFactura linea1 = new ItemFactura(producto1, 2);
+        ItemFactura linea2 = new ItemFactura(producto2, 4);
+        return Arrays.asList(linea1, linea2);
+    }
+
+    @Bean("itemsFacturaOficina")
+    @Primary
+    public List<ItemFactura> registrarItemsOficina() {
+        Producto producto1 = new Producto("Monitor LG LCD", 250);
+        Producto producto2 = new Producto("Notebook Asus", 500);
+        Producto producto3 = new Producto("Impresora HP", 80);
+        Producto producto4 = new Producto("Escritorio Oficina", 300);
+
+        ItemFactura linea1 = new ItemFactura(producto1, 2);
+        ItemFactura linea2 = new ItemFactura(producto2, 1);
+        ItemFactura linea3 = new ItemFactura(producto3, 1);
+        ItemFactura linea4 = new ItemFactura(producto4, 1);
+        return Arrays.asList(linea1, linea2, linea3, linea4);
     }
 }
--- src/main/java/com/bolsadeideas/springboot/di/app/models/domain/ItemFactura.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/ItemFactura.java
@@ -4,6 +4,11 @@ public class ItemFactura {
     private Producto producto;
     private Integer cantidad;
 
+    public ItemFactura(Producto producto, Integer cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
     public Producto getProducto() {
         return producto;
     }
@@ -19,4 +24,8 @@ public class ItemFactura {
     public void setCantidad(Integer cantidad) {
         this.cantidad = cantidad;
     }
+    public Integer calcularImporte(){
+        return cantidad*producto.getPrecio();
+    }
+
 }
